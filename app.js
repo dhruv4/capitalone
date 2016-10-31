871e67b49113e49ed488141218893836febd61da
Dhruv Gupta <dhruv4@gmail.com>
2016-10-31 03:53:34 -0400

    socket? Are you working? fr?

--- app.js
+++ app.js
@@ -9,18 +9,16 @@ var request = require("request");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+/*
 var app = require("express")();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(process.env.PORT || 8000);
+//server.listen(process.env.PORT || 8000);
+server.listen(7283);
+*/
 
-var nyt = require('newyorktimes')({ times_newswire: '20f4728f3b2f42c597ddc3030a24fd00' });
-
-var googleMapsClient = require('@google/maps').createClient({
-  key: 'AIzaSyCjHKGczt8QjINcTVQ4iZGXsSsYBESXipo'
-});
+const app = require('express')();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,77 +39,6 @@ app.locals.dataset = [
   [],
   []
 ];
-
-io.on('connection', function (socket) {
-
-  console.log("connected");
-
-  nyt.query('https://api.nytimes.com/svc/news/v3/content/all/all.json', function (err, json) {
-
-    var nytData = [];
-
-    json = JSON.parse(json["body"]);
-    for (var i = json['results'].length - 1; i >= 0; i--) {
-      if(json['results'][i]["geo_facet"]) {
-        console.log(json['results'][i]["geo_facet"][0]);
-        googleMapsClient.geocode({
-          address: json['results'][i]["geo_facet"][0]
-        }, function(err, response) {
-          if (!err) {
-            nytData.push(response.json.results[0].geometry.location['lat']);
-            nytData.push(response.json.results[0].geometry.location['lng']);
-            nytData.push(25);
-          }
-        });
-      }
-
-    };
-
-    io.emit('nyt', { data: JSON.stringify(nytData)} );
-    console.log("sent nyt", nytData);
-
-  });
-
-  request.get({
-    url: 'http://www.buzzfeed.com/api/v2/feeds/news',
-  }, function(err, response, body) {
-
-    var buzzData = [], buzzHeads = [];
-
-    body = JSON.parse(body);
-    
-    for (var i = body['big_stories'].length - 1; i >= 0; i--) {
-      if(body['big_stories'][i]['content_items'])
-        for (var j = body['big_stories'][i]['content_items'].length - 1; j >= 0; j--) {
-          if(body['big_stories'][i]['content_items'][j]['datelines'].length > 0){
-            //console.log(body['big_stories'][i]['content_items'][j]['datelines'][0]['marker_lng']);
-            buzzData.push(parseFloat(body['big_stories'][i]['content_items'][j]['datelines'][0]['marker_lat']));
-            buzzData.push(parseFloat(body['big_stories'][i]['content_items'][j]['datelines'][0]['marker_lng']));
-            buzzData.push(25);
-            buzzHeads.push(body['big_stories'][i]['content_items'][j]['title']);
-          }
-        };
-    };
-    for (var i = body['buzzes'].length - 1; i >= 0; i--) {
-      if(body['buzzes'][i]['datelines'].length > 0){
-        //console.log(body['buzzes'][i]['datelines'][0]['marker_lat']);
-        buzzData.push(parseFloat(body['buzzes'][i]['datelines'][0]['marker_lat']));
-        buzzData.push(parseFloat(body['buzzes'][i]['datelines'][0]['marker_lng']));
-        buzzData.push(25);
-        buzzHeads.push(body['buzzes'][i]['title']);
-      }
-    };
-
-    io.emit('buzz', { gldata: JSON.stringify(buzzData), heads: JSON.stringify(buzzHeads)} );
-
-  });
-
-
-  socket.on('disconnect', function () {
-    io.emit('user disconnected');
-  });
-});
-
 
 
 
